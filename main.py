4d5d89fb559edde5c24360cc297e6b019048db03
zpressley <107960313+zpressley@users.noreply.github.com>
2025-03-23 16:27:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "FBP API is live!"
+
+# Get PORT from environment or use 5000 locally
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
